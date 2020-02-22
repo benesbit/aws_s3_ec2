b0fc065473653ca9b293791e582ffb76cff6ae65
Ben Nesbit <benesbit@yahoo.com>
2020-02-22 12:32:43 -0800

    Add form for dynamo db upload

--- ec2_view/src/components/HelloWorld.vue
+++ ec2_view/src/components/HelloWorld.vue
@@ -2,6 +2,33 @@
   <div class="hello">
     <div class="card">
       <div class="card-content">
+        <form id="upload-to-db">
+          Upload new entry into DynamoDB
+          <br>
+          <p>
+            <label for="genre">Genre: </label>
+            <input id="genre" v-model="genre" type="text" name="genre">
+          </p>
+          <p>
+            <label for="artist">Artist: </label>
+            <input id="artist" v-model="artist" type="text" name="artist">
+          </p>
+          <p>
+            <label for="album">Album: </label>
+            <input id="album" v-model="album" type="text" name="album">
+          </p>
+          <p>
+            <label for="song">Song: </label>
+            <input id="song" v-model="song" type="text" name="song">
+          </p>
+          <p>
+            <input type="submit" value="Submit">
+          </p>
+        </form>
+      </div>
+    </div>
+    <div class="card">
+      <div class="card-content">
         <div v-for="(albumSongs, artist) in this.data" v-bind:key="artist">
           <Artists :ArtistName="artist" :AlbumSongs="albumSongs"/>
         </div>
@@ -28,9 +55,10 @@ export default {
     axios
       // .get('http://ec2-54-83-120-223.compute-1.amazonaws.com:3000/listEverything')
       // .get('http://localhost:3000/genres')
-      // .get('http://localhost:3000/artists/for/genre?genre=Rock')
-      // .get('http://localhost:3000/albums/for/artist?artist=Cake')
-      .get('http://localhost:3000/songs/for/album?album=Dude%20Ranch')
+      // .get('http://localhost:3000/artists/for/genre?genre=Game%20Music')
+      // .get('http://localhost:3000/albums/for/artist?artist=Zelda')
+      // .get('http://localhost:3000/songs/for/album?album=OoT')
+      .get('http://localhost:3000/song?song=Hyrule')
       .then(response => {
         console.log(response.data);
         this.data = response.data;
