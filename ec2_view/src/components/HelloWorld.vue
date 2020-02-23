f6908035a77fb0a6f5c36373aa59d966896a009a
Ben Nesbit <benesbit@yahoo.com>
2020-02-22 16:58:59 -0800

    Local test

--- ec2_view/src/components/HelloWorld.vue
+++ ec2_view/src/components/HelloWorld.vue
@@ -91,8 +91,8 @@ export default {
   mounted() {
     axios
       // .get('http://ec2-54-83-120-223.compute-1.amazonaws.com:3000/listEverything')
-      // .get('http://localhost:3000/genres')
-      .get('http://ec2-54-147-162-97.compute-1.amazonaws.com:3000/genres')
+      .get('http://localhost:3000/genres')
+      // .get('http://ec2-54-147-162-97.compute-1.amazonaws.com:3000/genres')
       // .get('http://localhost:3000/artists/for/genre?genre=Game%20Music')
       // .get('http://localhost:3000/albums/for/artist?artist=Zelda')
       // .get('http://localhost:3000/songs/for/album?album=OoT')
